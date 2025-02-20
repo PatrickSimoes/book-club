1deab31132d74c175f830e082fd79353ba8e73fc
Patrick Simões <patricksimoes25@gmail.com>
2025-02-19 22:27:18 -0300

    feat: Introduce a static book collection for the library

---
+++ src/database/books.js
@@ -0,0 +1,16 @@
+let books = [
+    {
+        id: 1,
+        title: "Comece pelo Porquê"
+    },
+    {
+        id: 2,
+        title: "Hábitos Atômicos"
+    },
+    {
+        id: 3,
+        title: "Essencialismo"
+    },
+];
+
+export default books;
