70c342f0db1215f4b297ccf87d13609563c3b7ce
Patrick Simões <patricksimoes25@gmail.com>
2025-02-20 21:59:51 -0300

    feat: Implement RESTful CRUD for book management using JavaScript object

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,32 @@
 import express from "express";
-import books from "../database/books.js";
+import bookService from "../services/BookService.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Hello World");
+    res.json(bookService.findAll());
 });
 
-router.get("/books", (req, res) => {
-    res.json(books);
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    res.json(bookService.findOne(id));
+});
+
+router.post("/", (req, res) => {
+    const bookData = req.body;
+
+    res.json(bookService.insert(bookData));
+});
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const bookData = req.body;
+    res.json(bookService.update(id, bookData));
+});
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    res.json(bookService.delete(id));
 });
 
 export default router;
